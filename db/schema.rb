4211cbb0407b4a85d8a05915a2641feeb60be9c0
shotawashino <shota.washino@gmail.com>
2020-07-27 11:43:32 +0900

    create association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_025433) do
+ActiveRecord::Schema.define(version: 2020_07_24_083844) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_07_24_025433) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "images", "items"
 end
