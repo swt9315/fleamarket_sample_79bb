bd746e85e474b569ba1eb2a9612101bdf28b04fa
swt9315 <67250386+swt9315@users.noreply.github.com>
2020-08-01 15:55:48 +0900

    S3導入関連の修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ここにバケット名を入れます'
+  config.fog_directory  = 'chatspace9315'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
 end
