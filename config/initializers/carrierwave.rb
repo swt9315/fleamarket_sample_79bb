26639788892227afa94f4acd07b9ab71f81e94af
swt9315 <67250386+swt9315@users.noreply.github.com>
2020-08-01 16:13:20 +0900

    S3導入の修正2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,9 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+    config.storage = :file
+  elsif Rails.env.production? #本番はS3に保存する
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -13,5 +16,6 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chatspace9315'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace9315'
+  end
 end
