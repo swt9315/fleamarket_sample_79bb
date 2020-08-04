2131a5ab4923af2467623ecc49605fdf4bf32d06
swt9315 <67250386+swt9315@users.noreply.github.com>
2020-08-04 16:22:23 +0900

    エラー修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,10 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
-    config.storage = :file
-  elsif Rails.env.production? #本番はS3に保存する
+  # if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+  #   config.storage = :file
+  # elsif Rails.env.production? #本番はS3に保存する
+  if Rails.env.production?
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -17,5 +18,9 @@ CarrierWave.configure do |config|
 
   config.fog_directory = 'chatspace9315'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace9315'
+
+   else
+    config.storage = :file
+    
   end
 end
