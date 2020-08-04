5d56f067ef5d12749c70c239c08dfae2aefe46bb
swt9315 <67250386+swt9315@users.noreply.github.com>
2020-08-04 15:31:22 +0900

    自動デプロイのエラー修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chatspace9315'
+  config.fog_directory = 'chatspace9315'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace9315'
   end
 end
