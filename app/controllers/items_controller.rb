f5d0c844922872d78132c54f2d3f4cdeaa0e82d8
shibatasuguru0311 <s.shibata.stuvwx@gmail.com>
2020-08-06 15:42:38 +0900

    sell-itemJS-create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,22 +12,34 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item=Item.create(item_params)
+    if  @item=Item.create!(item_params)
+    else
+      render :index
+    end
   end
 
   def show
+    Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
-  def destroy
-    @item.destroy
-    redirect_to root_path
+ 
+
+  def edit
+    @item = Item.find(params[:id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+  end
+
 
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :condition, :postage_user, :price, :preparation, :prefecture_id, :brand,images_attributes:[:image]).merge(seller_id: current_user.id)
+    params.require(:item).permit(:name, :introduction, :condition, :postage_user, :price, :preparation, :prefecture_id, :brand,images_attributes: [:image, :_destroy, :id]).merge(seller_id: current_user.id)
   end
 
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,5 @@ class Image < ApplicationRecord
   # carrierwaveでの記述
   mount_uploader :image, ImageUploader
   belongs_to :item
-
   validates :image, presence:true
 end
