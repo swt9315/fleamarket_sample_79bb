aef738de47c10a0c05f71eeafcd6c70915cff08c
swt9315 <67250386+swt9315@users.noreply.github.com>
2020-08-07 12:05:42 +0900

    【修正】destroy削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all
@@ -39,10 +39,6 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
-  end
-
-  def destroy
-    @item.destroy
   end
  
   private
