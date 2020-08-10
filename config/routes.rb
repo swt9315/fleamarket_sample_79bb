b49efac962a0576017648858c3faf34933f2cbeb
swt9315 <67250386+swt9315@users.noreply.github.com>
2020-08-10 15:15:26 +0900

    【修正】デプロイエラーによる修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+  
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
