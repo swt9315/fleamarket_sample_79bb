b8a8b51a8f8cde9bcc0aa5c29b981104dba77615
shibatasuguru0311 <s.shibata.stuvwx@gmail.com>
2020-08-11 15:57:58 +0900

    ation-modify

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,17 @@
+
 //= require rails-ujs
-  //= require turbolinks
-  //= require jquery
-  //= require bootstrap-sprockets
-  //= require_tree .
+//= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
+//= require_tree .
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 require("../category")
-require("../new_items")
+require("../new_items").start()
 require("../payjp")
-require('jquery')
+require('jquery').start()
+
+
+
