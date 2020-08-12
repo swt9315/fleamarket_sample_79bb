1aaecb93d9985e021f51c91b14d02d2dff9b97bc
shotawashino <shota.washino@gmail.com>
2020-08-12 21:16:52 +0900

    Correction of category and product image

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,9 @@
-
 //= require rails-ujs
-//= require turbolinks
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("channels")
 require("../category")
 require("../new_items")
